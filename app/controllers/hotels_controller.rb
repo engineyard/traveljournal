1e97e5f0b3f01f1e8358a9a3eed24f95984c23b5
Tim Gourley <tgourley@gmail.com>
2011-06-01 13:55:42 -0500

    Unit 7 Walkthrough 2

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -10,6 +10,25 @@ class HotelsController < ApplicationController
   def show
   end
 
+  def new
+    @cities = City.order(:name)
+    @hotel = Hotel.new
+  end
+
+  def create
+    @hotel = Hotel.new(params[:hotel])
+    if @hotel.save
+      flash[:notice] = "Hotel was created successfully."
+      respond_with(@hotel) do |format|
+        format.html { redirect_to @hotel }
+      end
+    else
+      flash[:error] = @hotel.errors.full_messages.join(", ")
+      @cities = City.order(:name)
+      render :action => :new
+    end
+  end
+
   protected
 
   def load_hotel
