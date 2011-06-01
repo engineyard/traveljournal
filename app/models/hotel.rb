1440dae3244507468a597ea31198102abf93c2a1
Tim Gourley <tgourley@gmail.com>
2011-06-01 09:31:26 -0500

    Unit 4 Walkthrough 4

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,2 +1,9 @@
 class Hotel < ActiveRecord::Base
+  validates :name, :presence => true
+  validates_length_of :name, :minimum => 2
+  before_save :ensure_name_is_in_titlecase
+
+  def ensure_name_is_in_titlecase
+    self.name = self.name.titlecase
+  end
 end
