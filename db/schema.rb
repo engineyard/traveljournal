c5c2aa4fd644fb76e1a1b9a6ce02ae5fc5e7062b
Tim Gourley <tgourley@gmail.com>
2011-06-01 09:41:57 -0500

    Unit 4 Walkthrough 5

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110531220558) do
+ActiveRecord::Schema.define(:version => 20110601144059) do
 
   create_table "cities", :force => true do |t|
     t.string   "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(:version => 20110531220558) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "city_id"
   end
 
 end
