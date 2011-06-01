f801d723e56c5af6a164cbb65c9afe89dc10338f
Tim Gourley <tgourley@gmail.com>
2011-06-01 12:11:43 -0500

    Unit 6 Lab

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,16 @@
 class TripsController < ApplicationController
+  before_filter :load_trips, :except => [ :new, :index, :create ]
+
+  def index
+    @trips = Trip.order(:name)
+  end
+
+  def show
+  end
+
+  protected
+
+  def load_trips
+    @trip = Trip.find(params[:id])
+  end
 end
