7388ed55a6852981a488bcdd72f301cfd12cc3be
Tim Gourley <tgourley@gmail.com>
2011-06-01 14:16:32 -0500

    Unit 7 Lab

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,4 +1,5 @@
 class TripsController < ApplicationController
+  respond_to :html, :xml, :json
   before_filter :load_trips, :except => [ :new, :index, :create ]
 
   def index
@@ -6,6 +7,25 @@ class TripsController < ApplicationController
   end
 
   def show
+  end
+
+  def new
+    @hotels = Hotel.order(:name)
+    @trip = Trip.new
+  end
+
+  def create
+    @trip = Trip.new(params[:trip])
+    if @trip.save
+      flash[:notice] = "Trip was created successfully."
+      respond_with(@trip) do |format|
+        format.html { redirect_to @trip }
+      end 
+    else
+      flash[:error] = @trip.errors.full_messages.join(", ")
+      @hotels = Hotel.order(:name)
+      render :action => :new
+    end
   end
 
   protected
